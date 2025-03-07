31a20423d01c7534962a8f811ae508d21408de56
KARTIKESH SINGH <kartikeshking02@gmail.com>
2025-03-08 00:05:25 +0530

    test

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+     <>
+     <div class="container mx-auto p-6 bg-white shadow-lg rounded-lg">
+        <h1 class="text-3xl font-bold text-center text-blue-600">Simple Tailwind Container</h1>
+        <p class="text-gray-700 text-center mt-2">
+            This is a responsive container using Tailwind CSS.
+        </p>
+    <div>
+      hy this is testing 
+    </div>
+    </div>
+    
+     </>
+  )
+}
+
+export default App
