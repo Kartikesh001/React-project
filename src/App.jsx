91fb822980468e2ee339385101df615ae6c7f78d
KARTIKESH SINGH <kartikeshking02@gmail.com>
2025-03-08 23:46:55 +0530

    fourth

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import LoginPage from "./login";
 
 function App() {
-  
-
   return (
-    
-     <>
-     <div class="container mx-auto p-6 bg-white shadow-lg rounded-lg">
-        <h1 class="text-3xl font-bold text-center text-blue-600">Simple Tailwind Container</h1>
-        <p class="text-gray-700 text-center mt-2">
-            This is a responsive container using Tailwind CSS.
-        </p>
-    <div>
-      hy this is testing testing now third time 
-       
-    </div>
-    </div>
-    
-     </>
-  )
+  <div>
+    <LoginPage/>
+ 
+  </div>
+  );
+  
 }
 
-export default App
+export default App;
