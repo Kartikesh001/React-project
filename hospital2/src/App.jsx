645dca25d43a55f5aa080ae4294a80f0cf64c13c
KARTIKESH SINGH <kartikeshking02@gmail.com>
2025-03-19 23:17:21 +0530

    fifth

---
+++ hospital2/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import LoginPage from "./Login";
+
+import Landing1 from "./Landing1";
+// import Landing from "./landing";
+// import Landing from "./Landing";
+function App() {
+  return (
+  <div>
+    
+    {/* <Landing/>
+    <Landing1/> */}
+<Landing1/>
+ 
+  </div>
+  );
+  
+}
+
+export default App;
